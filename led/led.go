7451e8280a335f18c168e5b3ca6d6e6651a38f31
Daniel Schaefer <46212174+Daniel-SchaeferJ@users.noreply.github.com>
2025-07-24 19:48:24 -0700

    Add UART logging to LED blinky example

--- led/led.go
+++ led/led.go
@@ -1,20 +1,25 @@
 package main
 
-// This is the most minimal blinky example and should run almost everywhere.
-
 import (
 	"machine"
+	"strconv"
 	"time"
 )
 
 func main() {
 	led := machine.LED
 	led.Configure(machine.PinConfig{Mode: machine.PinOutput})
+
+	machine.Serial.Configure(machine.UARTConfig{
+		BaudRate: 115200,
+	})
+	info_count := 0
 	for {
 		led.Low()
-
 		time.Sleep(time.Second)
-
+		machine.Serial.Write([]byte("Hello World!\n"))
+		info_count++
+		machine.Serial.Write([]byte("Info count: " + strconv.Itoa(info_count) + "\n"))
 		led.High()
 		time.Sleep(time.Second)
 	}
